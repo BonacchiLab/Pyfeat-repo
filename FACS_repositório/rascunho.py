1a20ebb79894fca237f97dc0ad05671e79e9e5a7
Maria S. Moreira <mariasilveiramoreira04@gmail.com>
2025-03-14 18:59:55 +0000

    file

--- FACS_repositório/rascunho.py
+++ FACS_repositório/rascunho.py
@@ -7,6 +7,7 @@ import os #module
 from feat import Fex #dataclass
 from IPython.core.display import Video #show video
 from feat.utils.io import read_feat #read csv file
+from pathlib import Path #get file path
 
 #Variáveis necessárias
 detector = Detector()
@@ -15,23 +16,23 @@ fex = Fex()
 # Get the full path
 folder_path = os.path.abspath("Input_Data")
 
-for dirpath, _, filenames in os.walk(folder_path):
-    for file in filenames:
-        if file.lower().endswith(('.jpg', '.png', '.jpeg')):  # add videos !!!
-            file_path = os.path.join(dirpath, file)
-            print(f"Processing: {file_path}")
-            
-            # Detectar face na imagem
-            single_face_prediction = detector.detect_image(file_path, data_type="image")
-            
-            # Exibir imagem
-            imshow(file_path)
-#pathlib!!!
+#for dirpath, _, filenames in os.walk(folder_path):
+#    for file in filenames:
+#        if file.lower().endswith(('.jpg', '.png', '.jpeg')):  # add videos !!!
+#            file_path = os.path.join(dirpath, file)
+#           print(f"Processing: {file_path}")           # Detectar face na imagem
+#            single_face_prediction = detector.detect_image(file_path, data_type="image")
+#            
+#            # Exibir imagem
+#            imshow(file_path)
+
+p = Path(folder_path)
+[x for x in p.iterdir() if x.is_dir()]
+
 
 
 #Exibir resultados 
-print(single_face_prediction) 
-print(single_face_prediction.head())
+print(single_face_prediction.head()) 
 print(single_face_prediction.aus) #actionunits
 print(single_face_prediction.emotions) #emotions
 print(single_face_prediction.poses) #head pose
