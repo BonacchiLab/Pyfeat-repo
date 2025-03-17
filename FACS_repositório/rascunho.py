970856818d9133fbc8503596507f90efe13528f4
Maria S. Moreira <mariasilveiramoreira04@gmail.com>
2025-03-17 16:31:49 +0000

    16/03

--- FACS_repositório/rascunho.py
+++ FACS_repositório/rascunho.py
@@ -14,24 +14,38 @@ detector = Detector()
 fex = Fex()
 
 # Get the full path
-folder_path = os.path.abspath("Input_Data")
-p = Path(folder_path)
-directories = [x for x in p.iterdir() if x.is_dir()]
+#folder_path = os.path.abspath("Input_Data")
+#p = Path(folder_path)
+#directories = [x for x in p.iterdir() if x.is_dir()]
+#q = p
+#with q.open() as f: f.readline()
+#p = PurePath('Input_Data')
+#os.fspath(p)
+#p = Path('Input_Data')
+#folder_path = [x for x in p.iterdir() if x.is_dir()]
+#with p.open() as f:
+#    f.readline()
 
+p = Path(r'C:\Users\Asus\Documents\recursos\estágio\FACS_repositório\Input_data').glob('*/')
+files = [x for x in p if x.is_file()]
 #detect single face 
-single_face_prediction=detector.detect_image(folder_path) 
+single_face_prediction=detector.detect_image(files, data_type="image") 
 
 
 
 #Exibir resultados 
-print(single_face_prediction.head()) 
-print(single_face_prediction.aus) #actionunits
-print(single_face_prediction.emotions) #emotions
-print(single_face_prediction.poses) #head pose
-print(single_face_prediction.identities) 
+for imagine in (folder_path):
+    print(single_face_prediction.head()) 
+    print(single_face_prediction.aus) #actionunits
+    print(single_face_prediction.emotions) #emotions
+    print(single_face_prediction.poses) #head pose
+    print(single_face_prediction.identities) 
 
 figs = single_face_prediction.plot_detections(poses=True)
 print(figs) #plot single image
+
+#Save output
+
 
 
 
--- FACS_repositório/scripts/trial_image.py
+++ FACS_repositório/scripts/trial_image.py
@@ -25,10 +25,9 @@ fex = Fex() # <-- Special dataframe that stores and operates on detector output
 
 #First analysis #
 single_face_prediction = detector.detect_image(single_face_img_path, data_type="image")
-single_face_prediction
 
 # Show results #
-print(single_face_prediction)
+
 print(single_face_prediction.head())
 print(single_face_prediction.aus) 
 print(single_face_prediction.emotions)
@@ -36,15 +35,16 @@ print(single_face_prediction.poses)
 print(single_face_prediction.identities)
 
 #Visualizing detection results
-#figs = single_face_prediction.plot_detections(poses=True)
+figs = single_face_prediction.plot_detections(poses=True)
 
-#print(figs)
+print(figs)
 
-#single_face_prediction.to_csv("output.csv", index=False)
+#save output
+single_face_prediction.to_csv("output.csv", index=False)
 
 from feat.utils.io import read_feat
 #retirar dados
-#input_prediction = read_feat("output.csv")
+input_prediction = read_feat("output.csv")
 
 #single_face_prediction.iplot_detections(bounding_boxes=True, emotions=True)
 
@@ -56,21 +56,21 @@ print(figs1)
 #(FEX OBJECT HAS NO ATTRIVUTE IPLOT_DETECTIONS)single_face_prediction.iplot_detections(bounding_boxes=True, emotions=True)
 
 ##MULTIPLE FACES##
-#multi_face_image_path = os.path.join(test_data_dir, "multi_face.jpg")
-#multi_face_prediction = detector.detect_image(multi_face_image_path, data_type="image")
+multi_face_image_path = os.path.join(test_data_dir, "multi_face.jpg")
+multi_face_prediction = detector.detect_image(multi_face_image_path, data_type="image")
 
 #Show results
-#print(multi_face_prediction)
+print(multi_face_prediction)
 
 #Visualizing detection results
-#figs2 = multi_face_prediction.plot_detections(add_titles=False)
+figs2 = multi_face_prediction.plot_detections(add_titles=False)
 
 ##MULTIPLE IMAGES##
-#img_list = [single_face_img_path, multi_face_image_path]
+img_list = [single_face_img_path, multi_face_image_path]
 
-#mixed_prediction = detector.detect_image(img_list, batch_size=1, data_type="image")
-#mixed_prediction
+mixed_prediction = detector.detect_image(img_list, batch_size=1, data_type="image")
+mixed_prediction
 
 #Visualizing detection results
-#figs3 = mixed_prediction.plot_detections()
-#print(figs3)
+figs3 = mixed_prediction.plot_detections()
+print(figs3)
