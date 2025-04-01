1f2282a910c6dca5aaacd433c57c27420873014a
Maria S. Moreira <mariasilveiramoreira04@gmail.com>
2025-04-01 12:37:16 +0100

    rascunho_01/04

--- FACS_repositório/rascunho.py
+++ FACS_repositório/rascunho.py
@@ -1,50 +1,50 @@
-#Importações necessárias
-from feat import Detector 
-import matplotlib.pyplot as plt #plot image if needed
-from feat.utils.io import get_test_data_path #path
-from feat.plotting import imshow #show image
-import os #module 
-from feat import Fex #dataclass
-from IPython.core.display import Video #show video
-from feat.utils.io import read_feat #read csv file
-from pathlib import Path #get file path
+# Importações necessárias
+from feat import Detector
+import matplotlib.pyplot as plt  # plot image if needed
+from feat.utils.io import get_test_data_path  # path
+from feat.plotting import imshow  # show image
+import os  # module
+from feat import Fex  # dataclass
+from IPython.core.display import Video  # show video
+from feat.utils.io import read_feat  # read csv file
+from pathlib import Path  # get file path
+import json
+from glob import glob
+import seaborn as sns
+from tqdm import tqdm
 
-#Variáveis necessárias
+# Inicialização do detector e Fex
 detector = Detector()
 fex = Fex()
 
-# Get the full path
-#folder_path = os.path.abspath("Input_Data")
-#p = Path(folder_path)
-#directories = [x for x in p.iterdir() if x.is_dir()]
-#q = p
-#with q.open() as f: f.readline()
-#p = PurePath('Input_Data')
-#os.fspath(p)
-#p = Path('Input_Data')
-#folder_path = [x for x in p.iterdir() if x.is_dir()]
-#with p.open() as f:
-#    f.readline()
 
-p = Path(r'C:\Users\Asus\Documents\recursos\estágio\FACS_repositório\Input_data').glob('*/')
-files = [x for x in p if x.is_file()]
-#detect single face 
-single_face_prediction=detector.detect_image(files, data_type="image") 
+# Diretório de dados de teste
+test_data_dir = get_test_data_path()
 
 
+# Caminho completo para a imagem de teste
+single_face_img_path = r"C:\Users\Asus\Downloads\single_face.jpg"
 
-#Exibir resultados 
-for imagine in (folder_path):
-    print(single_face_prediction.head()) 
-    print(single_face_prediction.aus) #actionunits
-    print(single_face_prediction.emotions) #emotions
-    print(single_face_prediction.poses) #head pose
-    print(single_face_prediction.identities) 
+# Caminho do vídeo de teste
+test_video_path = r"C:\Users\Asus\Downloads\single_face.mp4"
 
+# Plotar a imagem
+imshow(single_face_img_path)
+
+# Detectar características na imagem
+single_face_prediction = detector.detect_image(single_face_img_path, data_type="image")
+
+# Exibir resultados
+
+print(single_face_prediction.head())  # Exibir as primeiras linhas
+print(single_face_prediction.aus)  # Action Units
+print(single_face_prediction.emotions)  # Emoções
+print(single_face_prediction.poses)  # Head pose
+print(single_face_prediction.identities)  # Identidades
+
+# Plotar detecções com poses
 figs = single_face_prediction.plot_detections(poses=True)
-print(figs) #plot single image
-
-#Save output
+plt.show()  # Mostrar o gráfico gerado
 
 
 
--- FACS_repositório/scripts/trial_image.py
+++ FACS_repositório/scripts/trial_image.py
@@ -1,76 +1,65 @@
 from feat import Detector
 import matplotlib.pyplot as plt
-
-detector = Detector()
-
-## Processing a single image ##
-#Load the image
 from feat.utils.io import get_test_data_path
 from feat.plotting import imshow
 import os
-
-# Helper to point to the test data folder #
-test_data_dir = get_test_data_path()
-
-# Get the full path
-single_face_img_path = os.path.join(test_data_dir, "0-f1-su-ph.jpg")
-
-# Plot it
-imshow(single_face_img_path)
-
-#Import Fex Dataclass
 from feat import Fex
 
-fex = Fex() # <-- Special dataframe that stores and operates on detector output
+# Inicializar o detector
+detector = Detector()
 
-#First analysis #
+## Processando uma única imagem ##
+# Caminho da imagem
+single_face_img_path = r"C:\Users\Asus\Downloads\imagem_teste.png"
+
+# Exibir a imagem
+imshow(single_face_img_path)
+
+# Criar um objeto Fex
+fex = Fex()  # DataFrame especial que armazena e opera na saída do detector
+
+# Primeira análise
 single_face_prediction = detector.detect_image(single_face_img_path, data_type="image")
 
-# Show results #
-
+# Exibir resultados
 print(single_face_prediction.head())
-print(single_face_prediction.aus) 
+print(single_face_prediction.aus)
 print(single_face_prediction.emotions)
 print(single_face_prediction.poses)
 print(single_face_prediction.identities)
 
-#Visualizing detection results
+# Visualizar resultados de detecção
 figs = single_face_prediction.plot_detections(poses=True)
-
 print(figs)
 
-#save output
+# Salvar saída em um arquivo CSV
 single_face_prediction.to_csv("output.csv", index=False)
 
+# Ler os dados salvos
 from feat.utils.io import read_feat
-#retirar dados
 input_prediction = read_feat("output.csv")
 
-#single_face_prediction.iplot_detections(bounding_boxes=True, emotions=True)
-
-#Visualize a face using Py-Feat’s standardized AU landmark model
+# Visualizar usando o modelo de AU padronizado do Py-Feat
 figs1 = single_face_prediction.plot_detections(faces="aus", muscles=True)
 print(figs1)
 
-#Generate an interective ploty figure that lets you interectavly enable/disable detector outputs
-#(FEX OBJECT HAS NO ATTRIVUTE IPLOT_DETECTIONS)single_face_prediction.iplot_detections(bounding_boxes=True, emotions=True)
-
-##MULTIPLE FACES##
-multi_face_image_path = os.path.join(test_data_dir, "multi_face.jpg")
+## MULTIPLE FACES ##
+multi_face_image_path = os.path.join(get_test_data_path(), "multi_face.jpg")
 multi_face_prediction = detector.detect_image(multi_face_image_path, data_type="image")
 
-#Show results
+# Exibir resultados
 print(multi_face_prediction)
 
-#Visualizing detection results
+# Visualizar resultados de detecção
 figs2 = multi_face_prediction.plot_detections(add_titles=False)
 
-##MULTIPLE IMAGES##
+## MULTIPLE IMAGES ##
 img_list = [single_face_img_path, multi_face_image_path]
-
 mixed_prediction = detector.detect_image(img_list, batch_size=1, data_type="image")
-mixed_prediction
 
-#Visualizing detection results
+# Exibir resultados
+print(mixed_prediction)
+
+# Visualizar resultados de detecção
 figs3 = mixed_prediction.plot_detections()
 print(figs3)
